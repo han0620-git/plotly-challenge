775aaebc9293331afd24193d1abaaaf185292c2c
Han Zhao <han.zhao1989@gmail.com>
2020-11-17 22:47:12 -0600

    second commit

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -18,3 +18,47 @@ d3.json("samples.json").then((importedData) => {
   optionChanged(subject_ids[0]);
 });
 
+// The function is triggered by an option change in the Dropdown box of "Test Subject ID No" in index.html
+// <select id="selDataset" onchange="optionChanged(this.value)">
+function optionChanged(selected_id) {
+  console.log("selected_id=", selected_id);
+
+  d3.json("samples.json").then((data) => {
+    //   Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    // * Use`sample_values` as the values for the bar chart.
+    // * Use`otu_ids` as the labels for the bar chart.
+    // * Use`otu_labels` as the hovertext for the chart.
+
+    var samples = data.samples;
+    var results = samples.filter(sampleObj => sampleObj.id == selected_id);
+
+    console.log("samples: ");
+    console.log(samples);
+
+    var result = results[0];
+
+    console.log("result: ");
+    console.log(result);
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    var y_label = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var bar_trace = {
+      y: y_label,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+    };
+
+    var data = [bar_trace];
+
+    var bar_layout = {
+      title: "Top 10 OTUs",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", data, bar_layout);
+
